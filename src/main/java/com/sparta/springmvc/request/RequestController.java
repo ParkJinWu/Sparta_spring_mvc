3dcad39a7cee03b2c976b64157373d7e0bf81e72
ParkJinWu <onu0311@gmail.com>
2024-07-23 01:51:46 +0900

    Path Variable & Request Param

--- src/main/java/com/sparta/springmvc/request/RequestController.java
+++ src/main/java/com/sparta/springmvc/request/RequestController.java
@@ -3,14 +3,9 @@ package com.sparta.springmvc.request;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-/*
-    hello-request-form을 반환하는 Controller
-* */
-
 @Controller
 @RequestMapping("/hello/request")
 public class RequestController {
-
     @GetMapping("/form/html")
     public String helloForm() {
         return "hello-request-form";
@@ -18,7 +13,6 @@ public class RequestController {
 
     // [Request sample]
     // GET http://localhost:8080/hello/request/star/Robbie/age/95
-    // @PathVariable ➡️ URL 경로의 일부를 변수로 사용하여 매핑된 메서드에 전달할 수 있게 해주는 어노테이션
     @GetMapping("/star/{name}/age/{age}")
     @ResponseBody
     public String helloRequestPath(@PathVariable String name, @PathVariable int age)
@@ -28,19 +22,17 @@ public class RequestController {
 
     // [Request sample]
     // GET http://localhost:8080/hello/request/form/param?name=Robbie&age=95
-    // @RequestParam ➡️ 요청 URL에서 쿼리 파라미터를 매핑된 메서드의 매개변수로 전달하는 데 사용되는 어노테이션
     @GetMapping("/form/param")
     @ResponseBody
     public String helloGetRequestParam(@RequestParam(required = false) String name, int age) {
         return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
     }
 
-
     // [Request sample]
     // POST http://localhost:8080/hello/request/form/param
     // Header
     //  Content type: application/x-www-form-urlencoded
-    // Body(Payload)
+    // Body
     //  name=Robbie&age=95
     @PostMapping("/form/param")
     @ResponseBody
@@ -48,6 +40,35 @@ public class RequestController {
         return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
     }
 
+    // [Request sample]
+    // POST http://localhost:8080/hello/request/form/model
+    // Header
+    //  Content type: application/x-www-form-urlencoded
+    // Body
+    //  name=Robbie&age=95
+    @PostMapping("/form/model")
+    @ResponseBody
+    public String helloRequestBodyForm(@ModelAttribute Star star) {
+        return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
 
+    // [Request sample]
+    // GET http://localhost:8080/hello/request/form/param/model?name=Robbie&age=95
+    @GetMapping("/form/param/model")
+    @ResponseBody
+    public String helloRequestParam(@ModelAttribute Star star) {
+        return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
 
+    // [Request sample]
+    // POST http://localhost:8080/hello/request/form/json
+    // Header
+    //  Content type: application/json
+    // Body
+    //  {"name":"Robbie","age":"95"}
+    @PostMapping("/form/json")
+    @ResponseBody
+    public String helloPostRequestJson(@RequestBody Star star) {
+        return String.format("Hello, @RequestBody.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
 }
